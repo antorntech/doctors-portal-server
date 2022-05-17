478c9869bd7cc3fbba7c88804a3eac953d504394
antor10655gh <creativefreelancerantor@gmail.com>
2022-05-17 18:02:44 +0600

    add avalable api for available slots

--- index.js
+++ index.js
@@ -35,24 +35,29 @@ async function run() {
     });
 
     app.get("/available", async (req, res) => {
-      const date = req.query.date || "May 17, 2022";
+      const date = req.query.date;
 
       //   step-1: get all services
       const services = await serviceCollection.find().toArray();
 
-      //   step-2: get the booking of that day
+      //   step-2: get the booking of that day. output: [{},{},{},{},{}]
       const query = { date: date };
       const bookings = await bookingCollection.find(query).toArray();
 
       //   step-3: for each service, find bookings for that service
       services.forEach((service) => {
+        //   step-4: find bookings for that service. output: [{},{},{}]
         const serviceBookings = bookings.filter(
-          (b) => b.treatment === service.name
+          (book) => book.treatment === service.name
         );
-        const booked = serviceBookings.map((s) => s.slot);
-        const available = service.slots.filter((s) => !booked.includes(s));
-
-        service.available = available;
+        // step-5: select slots for the service Bookings: ['', '', '']
+        const bookedSlots = serviceBookings.map((book) => book.slot);
+        //step-6: select those slots that are not in bookedSlots
+        const available = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        // step-7: set available to slots to make it easier
+        service.slots = available;
       });
 
       res.send(services);
