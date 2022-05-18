c5648e9071a1f187479072cc17c421c420b6064f
antor10655gh <creativefreelancerantor@gmail.com>
2022-05-18 14:25:17 +0600

    add jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
@@ -46,7 +47,10 @@ async function run() {
         },
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ result, token });
     });
 
     app.get("/available", async (req, res) => {
